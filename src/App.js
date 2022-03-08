cd961b4b6de81ae8fb9b5ae7395864a3db1fbb58
Ruu92 <rubenbaronetto11@gmail.com>
2022-03-08 17:47:33 -0300

    create app layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-
+import Main from './components/main';
 function App() {
+const [userName, setUserName] = useState('Alex');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app-container">
+      <Main userName={userName}/>
+      </div>
+    
   );
 }
 
